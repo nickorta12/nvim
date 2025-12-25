02d56ad444cf81cfb1cf844101be3e766c7e3eab
Nicholas Orta <nickorta12@gmail.com>
2025-12-24 22:24:52 -0700

    Use latest ty

--- config/plugins/ty.nix
+++ config/plugins/ty.nix
@@ -1,88 +1,41 @@
 {
   lib,
   stdenv,
-  rustPlatform,
-  fetchFromGitHub,
-
-  # nativeBuildInputs
+  fetchurl,
   installShellFiles,
-
-  buildPackages,
-  versionCheckHook,
-  nix-update-script,
 }:
+let
+  inherit (stdenv.hostPlatform) system;
+  suffix =
+    {
+      "x86_64-linux" = "x86_64-unknown-linux-gnu.tar.gz";
+      "aarch64-darwin" = "aarch64-apple-darwin.tar.gz";
+    }
+    .${system} or (throw "Unsupported system: ${system}");
 
-rustPlatform.buildRustPackage (finalAttrs: {
+  hashes = {
+    "x86_64-linux" = "sha256-pvAagp0QVFpDfTycpVluztG0kRtmuYySZ2ejTkXUbGw=";
+    "aarch64-darwin" = "sha256-/hc7SVvo+WPSFkgFhsubyqb7CTAl0OrSsRx6QyO2zzE=";
+  };
+in
+stdenv.mkDerivation (finalAttrs: {
   pname = "ty";
-  version = "0.0.2";
+  version = "0.0.7";
 
-  src = fetchFromGitHub {
-    owner = "astral-sh";
-    repo = "ty";
-    tag = finalAttrs.version;
-    fetchSubmodules = true;
-    hash = "sha256-4Kv3UUn2QbOOOaQ/uzJB2KOUx++Gb+cKpZg4kGx99Q0=";
+  src = fetchurl {
+    url = "https://github.com/astral-sh/ty/releases/download/${finalAttrs.version}/ty-${suffix}";
+    hash = hashes.${system};
   };
 
-  # For Darwin platforms, remove the integration test for file notifications,
-  # as these tests fail in its sandboxes.
-  postPatch = lib.optionalString stdenv.hostPlatform.isDarwin ''
-    rm ${finalAttrs.cargoRoot}/crates/ty/tests/file_watching.rs
-  '';
-
-  cargoRoot = "ruff";
-  buildAndTestSubdir = finalAttrs.cargoRoot;
-
-  cargoBuildFlags = [ "--package=ty" ];
-
-  cargoHash = "sha256-nsrSAYRr80O0gNSzlV+IwD7tFbKogiimOfH3/O1Sqrk=";
+  dontBuild = true;
 
   nativeBuildInputs = [ installShellFiles ];
 
-  # `ty`'s tests use `insta-cmd`, which depends on the structure of the `target/` directory,
-  # and also fails to find the environment variable `$CARGO_BIN_EXE_ty`, which leads to tests failing.
-  # Instead, we specify the path ourselves and forgo the lookup.
-  # As the patches occur solely in test code, they have no effect on the packaged `ty` binary itself.
-  #
-  # `stdenv.hostPlatform.rust.cargoShortTarget` is taken from `cargoSetupHook`'s `installPhase`,
-  # which constructs a path as below to reference the built binary.
-  preCheck = ''
-    export CARGO_BIN_EXE_ty="$PWD"/target/${stdenv.hostPlatform.rust.cargoShortTarget}/release/ty
+  installPhase = ''
+    runHook preInstall
+    install -m755 -D ty $out/bin/ty
+    runHook postInstall
   '';
-
-  cargoTestFlags = [
-    "--package=ty" # CLI tests; file-watching tests only on Linux platforms
-    "--package=ty_python_semantic" # core type checking tests
-    "--package=ty_test" # test framework tests
-  ];
-
-  checkFlags = [
-    # Flaky:
-    # called `Result::unwrap()` on an `Err` value: Os { code: 26, kind: ExecutableFileBusy, message: "Text file busy" }
-    "--skip=python_environment::ty_environment_and_active_environment"
-    "--skip=python_environment::ty_environment_is_only_environment"
-    "--skip=python_environment::ty_environment_is_system_not_virtual"
-  ];
-
-  nativeInstallCheckInputs = [ versionCheckHook ];
-  versionCheckProgramArg = "--version";
-  doInstallCheck = true;
-
-  postInstall = lib.optionalString (stdenv.hostPlatform.emulatorAvailable buildPackages) (
-    let
-      emulator = stdenv.hostPlatform.emulator buildPackages;
-    in
-    ''
-      installShellCompletion --cmd ty \
-        --bash <(${emulator} $out/bin/ty generate-shell-completion bash) \
-        --fish <(${emulator} $out/bin/ty generate-shell-completion fish) \
-        --zsh <(${emulator} $out/bin/ty generate-shell-completion zsh)
-    ''
-  );
-
-  passthru = {
-    updateScript = nix-update-script { };
-  };
 
   meta = {
     description = "Extremely fast Python type checker and language server, written in Rust";
